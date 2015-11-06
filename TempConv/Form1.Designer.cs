52ef2b1f4ceda0a45a41131f208c652591da1109
dilek <dkoksal619@gmail.com>
2015-11-05 22:08:56 -0800

    TemperatureConverter

---
+++ TempConv/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace TempConv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblCelsius = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.closeButton.Location = new System.Drawing.Point(208, 312);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(105, 35);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "E&xit";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFahrenheit.Location = new System.Drawing.Point(1, 36);
+            this.lblFahrenheit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(178, 18);
+            this.lblFahrenheit.TabIndex = 1;
+            this.lblFahrenheit.Text = "Fahreneit Temperature";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(187, 30);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            212,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(139, 24);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            70,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lblCelsius
+            // 
+            this.lblCelsius.AutoSize = true;
+            this.lblCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelsius.Location = new System.Drawing.Point(1, 62);
+            this.lblCelsius.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCelsius.Name = "lblCelsius";
+            this.lblCelsius.Size = new System.Drawing.Size(164, 18);
+            this.lblCelsius.TabIndex = 3;
+            this.lblCelsius.Text = "Celcius Temperature";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.Location = new System.Drawing.Point(187, 62);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            212,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(139, 24);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(333, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(131, 22);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "use Farenheit";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(333, 62);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(117, 22);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "use Celsius";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 155);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(371, 112);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.lblCelsius);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.closeButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Temperature Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TempConv/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TempConv
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e) //void: nothing to return
+            //in VB: Sub btnStart_Click(sender As System.Object, e As System.EventArgs) sub does not return
+        {
+            this.Close();
+        }
+        
+        private double fToC(decimal fTemp)
+        {
+            return 50;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //get the current value, this following code is self documented code
+            calculateCValue();
+            addToListbox(numericUpDown1.Value, numericUpDown2.Value);
+        }
+
+        private void addToListbox(decimal fValue, decimal cValue)
+        {
+            listBox1.Items.Add(fValue + "F=" + cValue + "C");
+        }
+
+        private void calculateCValue()
+        {
+            decimal fValue = getCurrentValue(); //procedure, gets the currentValue sets to fvalue
+
+            //convert it
+            decimal cValue = convertoCelsius(fValue); //initialize cValue and convert function, sets the cValue converted below
+
+            //put in the other box
+            showInCBox(cValue);
+        }
+
+        private void showInCBox(decimal cValue)
+        {
+            numericUpDown2.Value = cValue;
+        }
+
+        private decimal convertoCelsius(decimal fValue) //procedure (mostly called function), converts it here
+        {
+            decimal cValue = (fValue - 32) * 5 / 9; //converts F to C
+            return cValue;
+        }
+
+        private decimal getCurrentValue() //function
+        {
+ 	    return numericUpDown1.Value;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calculateCValue();
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //if (radioButton2.Checked)
+            //{
+            //    numericUpDown1.Enabled =true;
+            //    numericUpDown2.Enabled =false;
+            //}
+            //    else{
+            //        numericUpDown1.Enabled =false;
+            //        numericUpDown2.Enabled =true;
+            //    }
+            numericUpDown1.Enabled = radioButton2.Checked; //above code optimized here
+            numericUpDown2.Enabled = !radioButton2.Checked;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = !radioButton1.Checked;
+            numericUpDown2.Enabled = radioButton1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+  
+        }
+    }
